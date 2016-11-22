5a1d0d2405acba9230a067bbdb3822da930777b3
fish444556 <jialinlei@hotmail.com>
2016-11-22 14:28:02 -0500

    3.8

---
+++ 3_8DontRepeatYourself.js
@@ -0,0 +1,52 @@
+
+/***************************** Q *****************************
+Right now we're hardcoding the URL of the form action in two places - in the HTML of the form, and in our 
+$.ajax call. Let's update our $.ajax call to use whatever URL the form specifies. Also, update the type of
+ the ajax request to use the method attribute of the form.
+ ***************************** Q *****************************/
+
+$(document).ready(function() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax('/book', {
+      type: 'POST',
+      data: $('form').serialize(),
+      dataType: 'json',
+      success: function(response) {
+        $('.tour').html('<p></p>')
+                  .find('p')
+                  .append('Trip to ' + response.description)
+                  .append(' at $' + response.price)
+                  .append(' for ' + response.nights + ' nights')
+                  .append('. Confirmation: ' + response.confirmation);
+      }
+    });
+  });
+});
+
+
+
+
+/***************************** A *****************************/
+
+
+$(document).ready(function() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax($('form').attr('action'), {
+      type: $('form').attr('method'),
+      data: $('form').serialize(),
+      dataType: 'json',
+      success: function(response) {
+        $('.tour').html('<p></p>')
+                  .find('p')
+                  .append('Trip to ' + response.description)
+                  .append(' at $' + response.price)
+                  .append(' for ' + response.nights + ' nights')
+                  .append('. Confirmation: ' + response.confirmation);
+      }
+    });
+  });
+});
+
+
